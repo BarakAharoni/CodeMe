287315d409b805323440416cf1e93250baf28277
RomiSalomon <118530977+RomiSalomon@users.noreply.github.com>
2023-03-28 19:14:19 +0300

    started to connect

--- public/jobOffer.js
+++ public/jobOffer.js
@@ -66,6 +66,49 @@ $('#home').on('click',(function (e) {
 
   
 
+//navbar ajax
+$('#home').on('click',(function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: 'GET',
+        url: '/',
+        dataType: 'text',
+        success: window.location = '/'
+    })
+  }));
+  
+  $('#devs').on('click',(function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: 'GET',
+        url: '/developers',
+        dataType: 'text',
+        success: window.location = '/developers'
+    })
+  }));
+  
+  $('#devsReg').on('click',(function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: 'GET',
+        url: '/developers/register',
+        dataType: 'text',
+        success: window.location = '/developers/register'
+    })
+  }));
+  
+  $('#job').on('click',(function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: 'GET',
+        url: '/jobOffers',
+        dataType: 'text',
+        success: window.location = '/jobOffers'
+    })
+  }));
+
+  
+
 const onClick = (e) => {
     var t = e.target;
     while (t && !t.id)
