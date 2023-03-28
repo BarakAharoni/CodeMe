dfa7b842977385d562de7ce57569586a81f43234
Barak Aharoni <97598628+BarakAharoni@users.noreply.github.com>
2023-03-28 12:09:54 +0300

    Job offers + creation

--- routes/jobOffer.js
+++ routes/jobOffer.js
@@ -1,10 +1,16 @@
 const express = require('express');
 var router = express.Router();
 const jobOfferController = require('../controllers/jobOffer');
+const JobOfferCreateController = require('../controllers/jobOfferCreate.js');
 
 router.route('/')
     .get(jobOfferController.getJobOffers)
     .post(jobOfferController.createJobOffer);
+
+router
+    .route('/create')
+    .get(JobOfferCreateController.getForm)
+    .post(JobOfferCreateController.createJobOffer);
 
 router.route('/:id')
     .get(jobOfferController.getJobOffer)
